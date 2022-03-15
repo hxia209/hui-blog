7885b6a819838badd99c5ed3ef16c3d49ce82b5b
hxia209 <huixia1990@gmail.com>
2022-03-15 21:08:42 +0000

    Fix broken links

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/blog">
+            A collection of a developer's daily struggles and how to solve them!
           </Link>
         </div>
       </div>
