58b080be5a20450d40485c28a2402e4a663e85ff
hxia209 <hxia@redhat.com>
2022-05-02 22:50:58 +0100

    Resize chart

--- src/pages/charts.js
+++ src/pages/charts.js
@@ -23,7 +23,7 @@ const data = [
 function RenderLineChart() {
     return (
         <Layout title="charts">
-            <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+            <LineChart width={1400} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
                 <Line type="linear" dataKey="water" stroke="#8884d8" />
                 <Line type="linear" dataKey="electricity" stroke="#FF0000" />
                 <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
