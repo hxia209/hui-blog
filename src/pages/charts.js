aa0fbd7f0c243527486596c5fb53463998af0991
hxia209 <hxia@redhat.com>
2022-05-02 22:43:56 +0100

    update stroke dash array

--- src/pages/charts.js
+++ src/pages/charts.js
@@ -26,7 +26,7 @@ function RenderLineChart() {
             <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
                 <Line type="linear" dataKey="water" stroke="#8884d8" />
                 <Line type="linear" dataKey="electricity" stroke="#FF0000" />
-                <CartesianGrid stroke="#ccc" strokeDasharray="1 1" />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                 <XAxis dataKey="name" />
                 <YAxis />
                 <Tooltip />
