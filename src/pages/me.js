3fed22263416762572d3af66f27f016685e639b1
hxia209 <huixia1990@gmail.com>
2022-05-30 22:38:14 +0100

    making about me page look, good'ish

--- src/pages/me.js
+++ src/pages/me.js
@@ -6,14 +6,22 @@ function me() {
   return (
     <Layout title="About me">
       <div class="card">
-        <div class="card-image">
-          <figure class="image is-512x512 ">
-            <img src={profile} alt="Profile Picture"/>
-          </figure>
-        </div>
-        <div class="card-content">
-          <p>Hello! My name is Hui and welcome to my blog.</p> 
-          <p>I'm an tech entusiast, open-source adopter, DevOps engineer, Java developer and newbie blogger.</p>
+        <div class="row">
+          <div class="column">
+            <div class="card-image">
+              <figure class="image is-512x512 ">
+                <img src={profile} alt="Profile Picture"/>
+              </figure>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card-content">
+              <div class="box">
+                <h3>Hello! My name is Hui and welcome to my blog.</h3> 
+                <p>I'm an tech entusiast, open-source adopter, DevOps engineer, Java developer and newbie blogger.</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
